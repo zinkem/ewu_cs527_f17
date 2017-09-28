4fbf861fa5356386f37c65926141ee1346da717b
zinkem <zinkem@gmail.com>
2017-09-28 13:51:57 -0700

    First commit with project structure

---
+++ index.js
@@ -0,0 +1,11 @@
+
+/*!
+ * Persistent Twitter Stream Storage
+ * Copyright(c) 2017 Matthew Zinke, Raymond Swannack
+ * Use at your own risk
+ */
+
+'use strict';
+
+module.exports = require('./lib/app');
+
