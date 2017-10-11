97a3ed1a17dc0707080a7a20f17e5866f2375dd9
zinkem <zinkem@gmail.com>
2017-10-11 01:44:54 -0700

    More tables, default http page shows queries

--- bin/schema_init.sql
+++ bin/schema_init.sql
@@ -3,9 +3,25 @@ CREATE DATABASE cs527;
 
 USE cs527;
 
-CREATE TABLE Tweets(
+CREATE TABLE tweets(
+       create_time int(8),
        user_name varchar(255),
        text varchar(255),
-       id int NOT NULL AUTO_INCREMENT,
+       id bigint NOT NULL,
        PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+       screen_name varchar(255) NOT NULL,
+       followers int DEFAULT 0,
+       friends int DEFAULT 0,
+       id int NOT NULL AUTO_INCREMENT,
+       PRIMARY KEY (id),
+       UNIQUE(screen_name)
+);
+
+CREATE TABLE hashtags(
+       label varchar(255) NOT NULL,
+       tweet_id bigint NOT NULL,
+       FOREIGN KEY (tweet_id) references tweets(id)
 );
